9bbc5b54ef378331bfbf4d6aadd84a2ab187b72a
luis-03 <87941524+luis-03@users.noreply.github.com>
2024-02-20 21:51:54 -0500

    Update CuentaController.js

--- controls/CuentaController.js
+++ controls/CuentaController.js
@@ -11,108 +11,113 @@ let jwt = require('jsonwebtoken');
 
 class CuentaController {
     async sesion(req, res) {
-        let errors = validationResult(req);
-        if (errors.isEmpty()) {
-            var login = await cuenta.findOne({
-                where: { correo: req.body.correo }, include: [{
-                    model: models.persona, as: 'persona',
-                    attributes: ['nombres', 'apellidos', 'id'],
-                }]
-            });
-            //console.log(login)
-            if (login === null) {
-                res.status(400);
-                
-                res.json({
-                    msg: "CUENTA NO ENCONTRADA",
-                    code: 400
+        try {
+            let errors = validationResult(req);
+            if (errors.isEmpty()) {
+                var login = await cuenta.findOne({
+                    where: { correo: req.body.correo }, include: [{
+                        model: models.persona, as: 'persona',
+                        attributes: ['nombres', 'apellidos', 'id'],
+                    }]
                 });
-                console.log(res.json);
-            } else {
-                //console.log(req.body.clave);
-                //var clave = req.body.clave;
-                //console.log(login.clave);
-                //var claveUs = login.clave;
+                //console.log(login)
+                if (login === null) {
+                    res.status(400);
 
-                console.log("SE HA ENCONTRADO UNA CUENTA, VERIFICANDO CONTRASENIA...");
-                res.status(200);
-                var isClavaValida = function (clave, claveUs) {
-                    //console.log(clave);
-                    //console.log(claveUs);
-                    return bcypt.compareSync(claveUs, clave);
-                };
-                console.log(isClavaValida(login.clave, req.body.clave))
+                    res.json({
+                        msg: "CUENTA NO ENCONTRADA",
+                        code: 400
+                    });
+                    console.log(res.json);
+                } else {
+                    //console.log(req.body.clave);
+                    //var clave = req.body.clave;
+                    //console.log(login.clave);
+                    //var claveUs = login.clave;
 
-                if (login.estado) {
-                    if (isClavaValida(login.clave, req.body.clave)) {
-                        console.log("CLAVE CORRECTA, GENERANDO TOKEN\n\n")
-                        const tokenData = {
-                            external: login.external_id,
-                            usuario: login.correo,
-                            rol: login.rol,
-                            check: true
-                        };
-                        require('dotenv').config();
-                        const llave = process.env.KEY;
-                        const token = jwt.sign(tokenData, llave);
-                        const cambios = {
-                            "Descripcion": "Inicio de sesion"
-                        }
-                        const datos = {
-                            "tipo_consulta": "LEER",
-                            "id_persona": login.id,
-                            "data": cambios
-                        }
-                        //await controlAcces.create(datos);              <----pendiente
-                        var nombreRol = Buffer(login.rol);
-                        nombreRol = nombreRol.toString('base64');
+                    console.log("SE HA ENCONTRADO UNA CUENTA, VERIFICANDO CONTRASENIA...");
+                    res.status(200);
+                    var isClavaValida = function (clave, claveUs) {
+                        //console.log(clave);
+                        //console.log(claveUs);
+                        return bcypt.compareSync(claveUs, clave);
+                    };
+                    console.log(isClavaValida(login.clave, req.body.clave))
 
-                        console.log("REGISTRANDO TOKEN EN BD\n\n")
-
-                        let errors = validationResult(req);
-                        if (errors.isEmpty()) {
-                            var data = {
-                                token: token,
-                                id_cuenta: login.id
+                    if (login.estado) {
+                        if (isClavaValida(login.clave, req.body.clave)) {
+                            console.log("CLAVE CORRECTA, GENERANDO TOKEN\n\n")
+                            const tokenData = {
+                                external: login.external_id,
+                                usuario: login.correo,
+                                rol: login.rol,
+                                check: true
                             };
-                            res.status(200);
-                            let transaction = await models.sequelize.transaction();
-                            try {
-                                await Token.create(data,transaction);
-                                console.log("TOKEN REGISTRADO CORRECTAMENTE\n\n")
-                                //await transaction.commit();
-                                //res.json({ msg: "Se ha registrado sus datos", code: 200 });
-                            } catch (error) {
-                                if (transaction) await transaction.rollback();
-                                if (error.errors && error.errors[0].message) {
-                                    //res.json({ msg: error.errors[0].message, code: 500 });
-                                    console.log({ msg: error.errors[0].message, code: 500 })
-                                } else {
-                                    //res.json({ msg: error.message, code: 500 });
-                                    console.log({ msg: error.errors[0].message, code: 500 })
+                            require('dotenv').config();
+                            const llave = process.env.KEY;
+                            const token = jwt.sign(tokenData, llave);
+                            const cambios = {
+                                "Descripcion": "Inicio de sesion"
+                            }
+                            const datos = {
+                                "tipo_consulta": "LEER",
+                                "id_persona": login.id,
+                                "data": cambios
+                            }
+                            //await controlAcces.create(datos);              <----pendiente
+                            var nombreRol = Buffer(login.rol);
+                            nombreRol = nombreRol.toString('base64');
+
+                            console.log("REGISTRANDO TOKEN EN BD\n\n")
+
+                            let errors = validationResult(req);
+                            if (errors.isEmpty()) {
+                                var data = {
+                                    token: token,
+                                    id_cuenta: login.id
+                                };
+                                res.status(200);
+                                let transaction = await models.sequelize.transaction();
+                                try {
+                                    await Token.create(data, transaction);
+                                    console.log("TOKEN REGISTRADO CORRECTAMENTE\n\n")
+                                    //await transaction.commit();
+                                    //res.json({ msg: "Se ha registrado sus datos", code: 200 });
+                                } catch (error) {
+                                    if (transaction) await transaction.rollback();
+                                    if (error.errors && error.errors[0].message) {
+                                        //res.json({ msg: error.errors[0].message, code: 500 });
+                                        console.log({ msg: error.errors[0].message, code: 500 })
+                                    } else {
+                                        //res.json({ msg: error.message, code: 500 });
+                                        console.log({ msg: error.errors[0].message, code: 500 })
+                                    }
                                 }
-                            } 
+                            }
+
+                            res.json({ msg: 'OK!', token: token, user: login.persona.nombres + ' ' + login.persona.apellidos, code: 200, correo: login.correo, iden: login.persona.identificacion, rol: nombreRol });
+
+
+                        } else {
+                            res.json({
+                                msg: "CLAVE INCORRECTA",
+                                code: 400
+                            });
                         }
-
-                        res.json({ msg: 'OK!', token: token, user: login.persona.nombres + ' ' + login.persona.apellidos, code: 200, correo: login.correo, iden: login.persona.identificacion, rol: nombreRol });
-
-
                     } else {
                         res.json({
-                            msg: "CLAVE INCORRECTA",
+                            msg: "CUENTA DESACTIVADA",
                             code: 400
                         });
                     }
-                } else {
-                    res.json({
-                        msg: "CUENTA DESACTIVADA",
-                        code: 400
-                    });
                 }
+            } else {
+                res.status(400);
+                res.json({ msg: "Faltan datos", code: 400 });
             }
-        } else {
-            res.status(400);
-            res.json({ msg: "Faltan datos", code: 400 });
+        } catch (error) {
+            res.status(500);
+            res.json({ msg: "Error de servidor", code: 500 });
         }
     }
 
@@ -126,7 +131,7 @@ class CuentaController {
                     code: 400
                 });
             } else {
-                
+
                 valCuenta.estado = !valCuenta.estado;
                 var result = await valCuenta.save();
                 if (result === null) {
@@ -146,7 +151,7 @@ class CuentaController {
         } catch (error) {
             res.status(400);
             res.json({
-                msg: "NO SE HAN MODIFICADO SUS DATOS: "+error,
+                msg: "NO SE HAN MODIFICADO SUS DATOS: " + error,
                 code: 500
             });
         }
